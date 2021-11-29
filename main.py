7a564657ec466251b3b12820267bab540274e37c
Rob Clinger <Robert7595@gmail.com>
2021-11-29 14:03:09 -0500

    split functions from main app to separate class for reusable methods.

--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-import characters
-
-def getCharacters():
-    # Create table for alphabet 
-    print("{0:5} | {1:^5}".format("Letter", "Morse Character"))
-    print("=======================")
-    for char in characters.alphabet:
-        print("{0:^6} | {1:^6}".format(char,characters.alphabet[str([char][0])]))
-    
-    # Create table for numbers
-    print("{0:5} | {1:^5}".format("Number", "Morse Number"))
-    print("=======================")
-    for num in characters.numbers:
-        print("{0:^6} | {1:^6}".format(num,characters.numbers[str([num][0])]))
-    print("")
-
-def morseEncode(word):
-    morseWord = []
-    for i in word:
-        if i in characters.alphabet:
-            for x in characters.alphabet:
-                if i == x:
-                    morseLetter = characters.alphabet[str([i][0])]
-                    morseWord.append(morseLetter)
-                else:
-                    pass
-        elif i in characters.numbers:
-            for x in characters.numbers:
-                if i == x:
-                    morseNum = characters.numbers[str([i][0])]
-                    morseWord.append(morseNum)
-                else:
-                    pass
-        else:
-            print("Could not translate.")
-    # Return the user input as Morse Code
-    print('Your message in Morse Code: ',*morseWord)
+from translator import Translator
 
 if __name__ == '__main__':
     
@@ -57,9 +21,9 @@ if __name__ == '__main__':
         if userChoice == "1":
             userInput = input("Enter a Letter, Number, Word, or Message: ").lower()
             customList = list(userInput)
-            morseEncode(customList)
+            Translator.morseEncode(customList)
         elif userChoice == "2":
-            getCharacters()
+            Translator.getCharacters()
         elif userChoice == "3":
             exit()
         else:
---
+++ translator.py
@@ -0,0 +1,42 @@
+import characters
+
+class Translator:
+
+    def __init__(self) -> None:
+        pass
+
+    def getCharacters():
+        # Create table for alphabet 
+        print("{0:5} | {1:^5}".format("Letter", "Morse Character"))
+        print("=======================")
+        for char in characters.alphabet:
+            print("{0:^6} | {1:^6}".format(char,characters.alphabet[str([char][0])]))
+        
+        # Create table for numbers
+        print("{0:5} | {1:^5}".format("Number", "Morse Number"))
+        print("=======================")
+        for num in characters.numbers:
+            print("{0:^6} | {1:^6}".format(num,characters.numbers[str([num][0])]))
+        print("")
+    
+    def morseEncode(word):
+        morseWord = []
+        for i in word:
+            if i in characters.alphabet:
+                for x in characters.alphabet:
+                    if i == x:
+                        morseLetter = characters.alphabet[str([i][0])]
+                        morseWord.append(morseLetter)
+                    else:
+                        pass
+            elif i in characters.numbers:
+                for x in characters.numbers:
+                    if i == x:
+                        morseNum = characters.numbers[str([i][0])]
+                        morseWord.append(morseNum)
+                    else:
+                        pass
+            else:
+                print("Could not translate.")
+        # Return the user input as Morse Code
+        print('Your message in Morse Code: ',*morseWord)
